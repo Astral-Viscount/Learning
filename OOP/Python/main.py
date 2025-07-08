45793e8f63be73ffa5dce6cefba170e8c609ddfd
Astral-Viscount <md.mahatabmahimn@gmail.com>
2025-07-08 23:01:31 +1200

    learned __init__

--- OOP/Python/main.py
+++ OOP/Python/main.py
@@ -1,17 +1,17 @@
 class Students:
+    def __init__(self, name, attendance, week):
+        self.name = name
+        self.attendance = attendance
+        self.week = week
+
+        print(f"Name: {self.name}\nAttendace per Week: {self.attendance}\nWeeks Attended: {self.week}")
+        
+        print(f"Total Days Attended: {self.total_attendance(self.attendance, self.week)}")
+
     def total_attendance(self, att, week):
         return att * week
 
 
-student1 = Students()
-student1.name = "Mahi"
-student1.attendance = 3
-student1.week = 34
-print(student1.total_attendance(student1.attendance, student1.week))
-
-
-student2 = Students()
-student2.name = "Nidhi"
-student2.attendance = 5
-student2.week = 23
-print(student2.total_attendance(student2.attendance, student2.week))
+student1 = Students("Mahi", 3, 34)
+print()
+student2 = Students("Nidhi", 5, 23)
