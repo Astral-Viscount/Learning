02b4bea5948ae6c7656c0005d8ac02c0334bfa8e
Astral-Viscount <md.mahatabmahimn@gmail.com>
2025-07-09 21:55:25 +1200

    simple things. (I lost motivation)

--- OOP/Python/main.py
+++ OOP/Python/main.py
@@ -1,17 +1,23 @@
 class Students:
-    def __init__(self, name, attendance, week):
+    def __init__(self, name:str, attendance: int, week = 0):
+
+        #Validate the values received with Custom Exception Errors
+        assert attendance >= 0, f"Attendance must be more than or equa 0 days! Attendance Received: {attendance}"
+        assert week >= 0, f"Must be more than or equal 0 Week! Week Received: {week}"
+
+        #Assign Values
         self.name = name
         self.attendance = attendance
         self.week = week
 
         print(f"Name: {self.name}\nAttendace per Week: {self.attendance}\nWeeks Attended: {self.week}")
         
-        print(f"Total Days Attended: {self.total_attendance(self.attendance, self.week)}")
+        print(f"Total Days Attended: {self.total_attendance()}")
 
-    def total_attendance(self, att, week):
-        return att * week
+    def total_attendance(self) -> int:
+        return self.attendance * self.week
 
 
-student1 = Students("Mahi", 3, 34)
+student1 = Students("Mahi", -3, 34)
 print()
 student2 = Students("Nidhi", 5, 23)
