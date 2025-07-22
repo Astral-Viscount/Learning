acad70f9f13d8c10a4fa94f0417ff6cd30bcf810
Astral-Viscount <md.mahatabmahimn@gmail.com>
2025-07-22 23:14:01 +1200

    learning js from w3 and bro code

--- Javascript/Basic/BroCode/script.js
+++ Javascript/Basic/BroCode/script.js
@@ -1,2 +1,33 @@
+/* Basics
+
 console.log(`Hello, World!`);
 window.alert("ALERT!!!");
+
+document.getElementById("hello").textContent = "Hello, World!";
+*/
+
+/* Variables
+
+let a;
+a = 100;
+
+let b = 200;
+
+console.log(`a + b = ${a + b}`);
+console.log(typeof a);
+*/
+
+/* Booleans
+
+let online = true;
+let for_sale = false;
+
+console.log(typeof online);
+
+*/
+
+/* With HTML */
+
+let name = "Mahi";
+
+document.getElementById("heading").textContent = `My name is ${name}`;
