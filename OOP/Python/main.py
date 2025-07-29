0f086f4bf3dad950401a9fddf6608c7eeb506ebf
Astral-Viscount <md.mahatabmahimn@gmail.com>
2025-07-29 21:26:33 +1200

    constructor, methods and attributes

--- OOP/Python/main.py
+++ OOP/Python/main.py
@@ -1,5 +1,6 @@
 class Students:
-    avg_rate = 0.8
+    avg_rate = 0.8 #On average students show up 80% of the time of the submiited time
+    everyone = []
 
     def __init__(self, name:str, attendance: int, week = 0):
 
@@ -12,20 +13,40 @@ class Students:
         self.attendance = attendance
         self.week = week
 
+        #Adding everyone
+        Students.everyone.append(self)
+
         print(f"Name: {self.name}\nAttendace per Week: {self.attendance}\nWeeks Attended: {self.week}")
-        
-        print(f"Total Days Attended: {self.total_attendance()}")
 
     def total_attendance(self) -> int:
-        return self.attendance * self.week
+        self.total = self.attendance * self.week
+        return self.total
+    
+    def apply_avg_rate(self) -> float:
+        return round(self.total_attendance() * self.avg_rate, 0)
+    
+    # Representing the values as close to as it was given to us
+    def __repr__(self) -> str:
+        return f"Students('{self.name}', {self.attendance}, {self.week})"
 
 
 student1 = Students("Mahi", 3, 34)
+print(f"Total days attended: {student1.apply_avg_rate()}")
+
 print()
+
 student2 = Students("Nidhi", 5, 23)
+student2.avg_rate = 0.5
+print(f"Total days attended: {student2.apply_avg_rate()}")
 
-print(Students.avg_rate)
-print(Students.__dict__) # All the class attributes
+print()
 
-print(student1.avg_rate)
-print(student1.__dict__) # All the instance attributes
+# Looking at the all the values
+print(Students.everyone)
+
+# # Different Attributes
+# print(Students.avg_rate)
+# print(Students.__dict__) # All the class attributes
+
+# print(student1.avg_rate)
+# print(student1.__dict__) # All the instance attributes
