abe0f148e10d080706a1dc79f3f51afbdc5dd6e3
Astral-Viscount <md.mahatabmahimn@gmail.com>
2025-07-29 20:34:45 +1200

    class attributes

--- OOP/Python/main.py
+++ OOP/Python/main.py
@@ -1,8 +1,10 @@
 class Students:
+    avg_rate = 0.8
+
     def __init__(self, name:str, attendance: int, week = 0):
 
         #Validate the values received with Custom Exception Errors
-        assert attendance >= 0, f"Attendance must be more than or equa 0 days! Attendance Received: {attendance}"
+        assert attendance >= 0, f"Attendance must be more than or equal to 0 days! Attendance Received: {attendance}"
         assert week >= 0, f"Must be more than or equal 0 Week! Week Received: {week}"
 
         #Assign Values
@@ -18,6 +20,12 @@ class Students:
         return self.attendance * self.week
 
 
-student1 = Students("Mahi", -3, 34)
+student1 = Students("Mahi", 3, 34)
 print()
 student2 = Students("Nidhi", 5, 23)
+
+print(Students.avg_rate)
+print(Students.__dict__) # All the class attributes
+
+print(student1.avg_rate)
+print(student1.__dict__) # All the instance attributes
