f24aa3e317045fa5ba134d52b92dbfbf1434cddf
Astral-Viscount <md.mahatabmahimn@gmail.com>
2026-03-02 23:56:08 +1300

    got the full fundamental syntax of rust

--- Rust/W3School/src/main.rs
+++ Rust/W3School/src/main.rs
@@ -157,5 +157,133 @@ fn main() {
     println!("{:?}", numbers); //Prints the entire array
 
     //Vectors
-    
-}
+    let mut fruits = vec!["apple", "banana", "orange"];
+    fruits[0] = "grape";
+    println!("New first fruit: {}", fruits[0]);
+
+    fruits.push("cherry");
+
+    fruits.pop();
+
+    fruits.insert(0, "apple");
+    fruits.remove(0);
+
+    println!("There are {} fruits.", fruits.len());
+
+    let fruits = vec!["apple", "banana", "orange"];
+    for fruit in &fruits { // Use &fruits to borrow the vector instead of moving it
+        println!("I like {}.", fruit);
+    }
+
+    //Tuple
+    let person = ("John", 30, true);
+    println!("Name: {}", person.0);
+    println!("Age: {}", person.1);
+    println!("Is active: {}", person.2);
+
+    //Unpacking a tuple
+    let person = ("Jenny", 45, false);
+    let (name, age, active) = person;
+
+    println!("Name: {}", name);
+    println!("Age: {}", age);
+    println!("Active: {}", active)
+
+    //HashMap 
+    // Import HashMap before main func
+    use std::collections::HashMap;
+
+    fn main() {
+        // Create a HashMap called capitalCities
+        let mut capitalCities = HashMap::new();
+
+        // Add keys and values (Country, City)
+        //HashMaps require keys to be unique. Inserting the same key again will overwrite the old value.
+        capitalCities.insert("England", "London");
+        capitalCities.insert("Germany", "Berlin");
+        capitalCities.insert("Norway", "Oslo");
+
+        println!("{:?}", capitalCities);
+    }
+
+    //Accessing values
+    let mut capitalCities = HashMap::new();
+
+    capitalCities.insert("England", "London");
+    capitalCities.insert("Germany", "Berlin");
+    capitalCities.insert("Norway", "Oslo");
+
+    if let Some(city) = capitalCities.get("England") {
+        println!("The capital of England is {}.", city);
+    } else {
+        println!("England is not in the map.");
+    }
+
+    //Updating values
+    let mut capitalCities = HashMap::new();
+
+    capitalCities.insert("England", "London");
+    capitalCities.insert("England", "Berlin");
+
+    println!("{:?}", capitalCities);
+
+    //Removing values
+    let mut capitalCities = HashMap::new();
+
+    // Add keys and values (Country, City)
+    capitalCities.insert("England", "London");
+    capitalCities.insert("Germany", "Berlin");
+    capitalCities.insert("Norway", "Oslo");
+
+    // Remove the key "England"
+    capitalCities.remove("England");
+
+    println!("{:?}", capitalCities);
+
+    //Looping
+    let mut capitalCities = HashMap::new();
+
+    // Add keys and values (Country, City)
+    capitalCities.insert("England", "London");
+    capitalCities.insert("Germany", "Berlin");
+    capitalCities.insert("Norway", "Oslo");
+
+    // Loop through the HashMap
+    for (country, city) in &capitalCities {
+        println!("The capital of {} is {}.", country, city);
+    }
+
+    //Structs
+    //You can think of a struct like a mini-database for one thing, like a person with a name and age
+    struct Person {
+    name: String,
+    age: u32,
+    }
+
+    let mut user = Person {
+    name: String::from("John"),
+    age: 35,
+    };
+
+    user.age = 36; // Change value of age
+    println!("Name: {}", user.name);
+    println!("Updated age: {}", user.age);
+
+    //Enum 
+    /* Enums are useful when you want to represent a value that can only be one of a set of options
+       - like days of the week, directions, or results like success and error.*/
+    enum LoginStatus {
+    Success(String),
+    Error(String),
+    }
+
+    fn main() {
+        let result1 = LoginStatus::Success(String::from("Welcome, John!"));
+        let result2 = LoginStatus::Error(String::from("Incorrect password"));
+
+        match result1 {
+            LoginStatus::Success(message) => println!("Success: {}", message),
+            LoginStatus::Error(message) => println!("Error: {}", message),
+        }
+    }
+}   
