5ca67c8813fb208f0f83d645aa2e9ff334d92fe6
Astral-Viscount <md.mahatabmahimn@gmail.com>
2026-03-05 22:19:42 +1300

    guessing game

--- Rust/Interactive/guessing_game/src/main.rs
+++ Rust/Interactive/guessing_game/src/main.rs
@@ -1,13 +1,37 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess:");
+    let number = rand::thread_rng().gen_range(1..=100);
+    // println!("Number: {number}");
 
-    let mut guess= String::new();
+    loop {
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Error");
+        let mut guess= String::new();
 
-    println!("Your guessed number: {guess}")
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Error");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please input a number: ");
+                continue;
+            }
+        };
+
+        println!("Your guessed number: {guess}");
+
+        match guess.cmp(&number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You guessed it! You win!");
+                break;
+            }
+        }
+    }
 }
