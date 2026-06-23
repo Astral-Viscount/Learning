54c55be13640715b4bdb45b89d210ce48d5b506e
Astral-Viscount <md.mahatabmahimn@gmail.com>
2026-06-23 22:10:11 +1200

    control flows and loops

---
+++ Rust/Interactive/control_flows/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let number = 6;
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+
+    let condition = true;
+    let number = if condition { 5 } else { 6 }; //values that have the potential to be results from each arm of the if must be the same type
+
+    println!("The value of number is: {number}");
+}
